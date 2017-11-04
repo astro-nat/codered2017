94c43df265fdd18622cee7e2665d8d450d2d277a
Faheem Quazi <faheem@quazi.me>
2017-11-04 17:44:27 -0500

    Read all fingers + Bluetooth dump

--- flex_arduino/flex/flex.ino
+++ flex_arduino/flex/flex.ino
@@ -1,20 +1,40 @@
-const int flexpin = 0;
+#include <SoftwareSerial.h>
+
+// Bluetooth Module TX->10
+//                  RX->11
+SoftwareSerial btComm(10, 11); // syntax: (RX, TX)
+
+// Flex Sensors
+const int f_index  = 0; // A0 = Index
+const int f_middle = 1; // A1 = Middle
+const int f_ring   = 2; // A2 = Ring
+const int f_pinky  = 3; // A3 = Pinky
+const int f_thumb  = 4; // A4 = Thumb
+
+const int dpps = 50; // data-points per second
 
 void setup() {
-  // Begin Serial Monitoring
+  // Begin PC Serial Communication
   Serial.begin(9600);
+
+  // Begin Bluetooth Serial Commmunication
+  btComm.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int flexposition;    // Input value from the analog pin.
-  int servoposition;   // Output value to the servo.
 
-  // Read the position of the flex sensor (0 to 1023):
+  // Read the position of the flex sensors (~512 to ~1024):
+  int index = analogRead(f_index);
+  int middle = analogRead(f_middle);
+  int ring = analogRead(f_ring);
+  int pinky = analogRead(f_pinky);
+  int thumb = analogRead(f_thumb);
+  
+  // form telemetry string
+  String tlm = String(index) + "," + String(middle) + "," + String(ring) + "," + String(pinky) + "," + String(thumb);
 
-  flexposition = analogRead(flexpin);
+  btComm.println(tlm); // send to bluetooth
+  Serial.println(tlm); // send to serial
 
-  Serial.println(flexposition);
-
-  delay(20);
+  delay(1000/dpps);
 }
